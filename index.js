0fb833cc2067f2d7cd9240492bc688170fa7c649
Victor Kauan <victorkauan.dev@gmail.com>
2022-10-27 21:49:39 -0300

    initial: create JavaScript function file

---
+++ index.js
@@ -0,0 +1,29 @@
+// Sum 2 (two) numbers
+function sum() {
+
+}
+
+// Subtract 2 (two) numbers
+function subtraction() {
+  
+}
+
+// Divide 2 (two) numbers
+function division() {
+
+}
+
+// Multiplicate 2 (two) numbers
+function multiplication() {
+
+}
+
+// Floor a number
+function floor() {
+
+}
+
+// Ceil a number
+function ceil() {
+
+}
